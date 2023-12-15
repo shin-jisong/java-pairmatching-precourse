cc287c5267ab23710d961ede9abd805ecce5298f
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 16:26:17 +0900

    feat: add constructor to crew model

--- src/main/java/pairmatching/model/Crew.java
+++ src/main/java/pairmatching/model/Crew.java
@@ -1,4 +1,14 @@
 package pairmatching.model;
 
+import java.util.List;
+
 public class Crew {
+
+    private final List<String> backendCrew;
+    private final List<String> frontendCrew;
+
+    public Crew(List<String> backendCrew, List<String> frontendCrew) {
+        this.backendCrew = backendCrew;
+        this.frontendCrew = frontendCrew;
+    }
 }
