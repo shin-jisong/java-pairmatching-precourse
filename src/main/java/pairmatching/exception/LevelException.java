957d04f92a2da3af85d2e096afb10ddd61112f09
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 16:15:14 +0900

    feat: add invalid mission to level exception

--- src/main/java/pairmatching/exception/LevelException.java
+++ src/main/java/pairmatching/exception/LevelException.java
@@ -1,7 +1,8 @@
 package pairmatching.exception;
 
 public enum LevelException {
-    INVALID_LEVEL("유효하지 않은 레벨입니다. 다시 입력해 주세요.");
+    INVALID_LEVEL("유효하지 않은 레벨입니다. 다시 입력해 주세요."),
+    INVALID_MISSION("유효하지 않은 미션입니다. 다시 입력해 주세요.");
 
     private final String exceptionMessage;
 
