0d65f6f2661349a790b6cef738342949e7cf4c7c
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 16:57:11 +0900

    feat: add constructor to pair model

--- src/main/java/pairmatching/model/Pair.java
+++ src/main/java/pairmatching/model/Pair.java
@@ -1,4 +1,18 @@
 package pairmatching.model;
 
+import java.util.List;
+
 public class Pair {
+    private final Course course;
+    private final Level level;
+    private final Crew crew;
+    private List<String> crews;
+
+    public Pair(String course, String level, String mission) {
+        this.course = new Course(course);
+        this.level = new Level(level, mission);
+        this.crew = new Crew();
+    }
+
+    
 }
