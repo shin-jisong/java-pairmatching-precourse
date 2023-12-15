1548a0ce35465c7b4e8ea66a9f99a7741d8921db
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 15:57:41 +0900

    feat: add validate course function to course model

--- src/main/java/pairmatching/model/Course.java
+++ src/main/java/pairmatching/model/Course.java
@@ -2,6 +2,7 @@ package pairmatching.model;
 
 import java.util.Arrays;
 import java.util.List;
+import pairmatching.exception.CourseException;
 
 public class Course {
 
@@ -9,11 +10,18 @@ public class Course {
     private final String currentCourse;
 
     public Course(String currentCourse) {
+        validateCourse(currentCourse);
         this.currentCourse = currentCourse;
     }
 
     public boolean compareCourse(String targetCourse) {
         return currentCourse.equals(targetCourse);
+    }
+
+    private void validateCourse(String currentCourse) {
+        if(!courseName.contains(currentCourse)) {
+            throw new IllegalArgumentException(CourseException.INVALID_COURSE.getExceptionMessage());
+        }
     }
 
 }
