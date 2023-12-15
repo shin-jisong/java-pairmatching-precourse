ead0219df2e8af5db6441b4a5ed5ead02fca29b6
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 15:50:47 +0900

    feat: add constructor to course model

--- src/main/java/pairmatching/model/Course.java
+++ src/main/java/pairmatching/model/Course.java
@@ -1,4 +1,15 @@
 package pairmatching.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Course {
+
+    private final List<String> courseName = Arrays.asList("백엔드", "프론트엔드");
+    private final String currentCourse;
+
+    public Course(String currentCourse) {
+        this.currentCourse = currentCourse;
+    }
+
 }
