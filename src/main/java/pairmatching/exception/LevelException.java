386025bb7a949c70b628cc3cb5843e9775ce5465
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 16:14:27 +0900

    feat: add invalid level to level exception

--- src/main/java/pairmatching/exception/LevelException.java
+++ src/main/java/pairmatching/exception/LevelException.java
@@ -1,4 +1,15 @@
 package pairmatching.exception;
 
-public class LevelException {
+public enum LevelException {
+    INVALID_LEVEL("유효하지 않은 레벨입니다. 다시 입력해 주세요.");
+
+    private final String exceptionMessage;
+
+    LevelException(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public String getExceptionMessage() {
+        return this.exceptionMessage;
+    }
 }
