80951a152ba453d451649078c4527d93f2f13076
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 16:46:16 +0900

    feat: add read file function to crew model

--- src/main/java/pairmatching/model/Crew.java
+++ src/main/java/pairmatching/model/Crew.java
@@ -1,14 +1,29 @@
 package pairmatching.model;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import pairmatching.Application;
 
 public class Crew {
 
     private final List<String> backendCrew;
     private final List<String> frontendCrew;
 
-    public Crew(List<String> backendCrew, List<String> frontendCrew) {
-        this.backendCrew = backendCrew;
-        this.frontendCrew = frontendCrew;
+    public Crew() {
+        this.backendCrew = readCrew("backend-crew.md");
+        this.frontendCrew = readCrew("frontend-crew.md");
+    }
+
+    private List<String> readCrew(String crewCourse) {
+        List<String> lines = new ArrayList<>();
+        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(crewCourse);
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            lines.add(line);
+        }
+        return lines;
     }
 }
