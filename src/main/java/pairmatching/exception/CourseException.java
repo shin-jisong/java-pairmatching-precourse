ed46ef96eacd4db581c7b30ec150fb49604c99a4
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 15:57:21 +0900

    feat: add invalid course to course exception

--- src/main/java/pairmatching/exception/CourseException.java
+++ src/main/java/pairmatching/exception/CourseException.java
@@ -1,4 +1,16 @@
 package pairmatching.exception;
 
-public class CourseException {
+public enum CourseException {
+
+    INVALID_COURSE("유효하지 않은 과정입니다. 다시 입력해 주세요.");
+
+    private final String exceptionMessage;
+
+    CourseException(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public String getExceptionMessage() {
+        return this.exceptionMessage;
+    }
 }
